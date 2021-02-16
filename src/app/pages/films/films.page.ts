66aaa80a403f7782eb7c120586a7424be136ae17
rsurichaqui <rsurichaquim@hotmail.com>
2021-02-15 22:45:51 -0500

    first commit

---
+++ src/app/pages/films/films.page.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+//import { NavController } from '@ionic/angular';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-films',
+  templateUrl: './films.page.html',
+  styleUrls: ['./films.page.scss'],
+})
+export class FilmsPage implements OnInit {
+
+  films: Observable<any>;
+
+  constructor(private router: Router, private http: HttpClient) { }
+
+  ngOnInit() {
+    this.films = this.http.get('https://swapi.dev/api/films');
+    this.films.subscribe(data => {
+      console.log('my data: ', data);
+    });
+  }
+  openDetails(film) {
+    let split = film.url.split('/');
+    let filmId = split[split.length-2];
+    this.router.navigateByUrl(`/tabs/films/${filmId}`);
+  }
+}
